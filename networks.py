5d6b83b28bdf141abc2de0d918cd5751333bd581
Hotaek Han <hotaekhan@vtouch.kr>
2017-03-29 11:01:59 +0900

    1. bug fix

--- networks.py
+++ networks.py
@@ -22,31 +22,42 @@ import tensorflow as tf
 
 
 # DCGAN
-def generator(z):
-    net = slim.fully_connected(z, 16384, activation_fn=None)
-    net = tf.reshape(net, [-1, 4, 4, 1024])
-    net = slim.conv2d_transpose(net, 512, [5, 5], stride=2, padding='SAME', activation_fn=slim.nn.relu)
-    net = slim.batch_norm(net)
-    net = slim.conv2d_transpose(net, 256, [5, 5], stride=2, padding='SAME', activation_fn=slim.nn.relu)
-    net = slim.batch_norm(net)
-    net = slim.conv2d_transpose(net, 128, [5, 5], stride=2, padding='SAME', activation_fn=slim.nn.relu)
-    net = slim.batch_norm(net)
-    g_image = slim.conv2d_transpose(net, 1, [5, 5], stride=2, padding='SAME', activation_fn=slim.nn.tanh)
+def generator(z, is_train=True, is_reuse=False):
+
+    with tf.variable_scope("generator", reuse=is_reuse):
+
+        net = slim.fully_connected(z, 16384, activation_fn=None)
+        net = tf.reshape(net, [-1, 4, 4, 1024])
+        net = slim.batch_norm(net, activation_fn=slim.nn.relu, is_training=is_train)
+        net = slim.conv2d_transpose(net, 512, [5, 5], stride=2, padding='SAME', activation_fn=None)
+        net = slim.batch_norm(net, activation_fn=slim.nn.relu, is_training=is_train)
+        net = slim.conv2d_transpose(net, 256, [5, 5], stride=2, padding='SAME', activation_fn=None)
+        net = slim.batch_norm(net, activation_fn=slim.nn.relu, is_training=is_train)
+        net = slim.conv2d_transpose(net, 128, [5, 5], stride=2, padding='SAME', activation_fn=None)
+        net = slim.batch_norm(net, activation_fn=slim.nn.relu, is_training=is_train)
+        logits = slim.conv2d_transpose(net, 1, [5, 5], stride=2, padding='SAME', activation_fn=None)
+        g_image = tf.nn.tanh(logits)
 
     return g_image
 
 
-def discriminator(sample):
-    net = slim.conv2d(sample, 128, [5, 5], stride=2, padding='SAME', activation_fn=slim.nn.relu)
-    net = slim.batch_norm(net)
-    net = slim.conv2d(net, 256, [5, 5], stride=2, padding='SAME', activation_fn=slim.nn.relu)
-    net = slim.batch_norm(net)
-    net = slim.conv2d(net, 512, [5, 5], stride=2, padding='SAME', activation_fn=slim.nn.relu)
-    net = slim.batch_norm(net)
-    net = slim.conv2d(net, 1024, [4, 4], stride=2, padding='SAME', activation_fn=slim.nn.relu)
-    net = slim.batch_norm(net)
-    net = slim.conv2d(net, 1, [4, 4], stride=1, padding='VALID', activation_fn=slim.nn.relu)
-    logit = tf.reshape(net, [-1, 1])
-    prob = slim.nn.sigmoid(logit)
+def discriminator(sample, is_train=True, is_reuse=False):
+
+    with tf.variable_scope("discriminator", reuse=is_reuse):
+
+        net = slim.conv2d(sample, 64, [5, 5], stride=2, padding='SAME', activation_fn=slim.nn.relu)
+        # net = slim.batch_norm(net)
+        net = slim.conv2d(net, 128, [5, 5], stride=2, padding='SAME', activation_fn=None)
+        net = slim.batch_norm(net, activation_fn=slim.nn.relu, is_training=is_train)
+        net = slim.conv2d(net, 256, [5, 5], stride=2, padding='SAME', activation_fn=None)
+        net = slim.batch_norm(net, activation_fn=slim.nn.relu, is_training=is_train)
+        net = slim.conv2d(net, 512, [4, 4], stride=2, padding='SAME', activation_fn=None)
+        net = slim.batch_norm(net, activation_fn=slim.nn.relu, is_training=is_train)
+        # net = slim.conv2d(net, 1, [4, 4], stride=1, padding='VALID', activation_fn=slim.nn.relu)
+        net = slim.flatten(net)
+        logit = slim.fully_connected(net, 1, activation_fn=None)
+
+        # logit = tf.reshape(net, [-1, 1])
+        prob = slim.nn.sigmoid(logit)
 
     return prob, logit
--- train.py
+++ train.py
@@ -29,13 +29,14 @@ def plot(samples):
 def main():
 
     log_dir = "log/model_without_noise.ckpt"
-    train_dir_path = "D:\\Projects\\RFR\\Dataset\\HUD_1st\\converted\\Training"
+    # train_dir_path = "D:\\Projects\\RFR\\Dataset\\HUD_1st\\converted\\Training"
+    train_dir_path = "C:\\Users\\VTouch\\Documents\\Experiment\\TensorFlow\\DCGAN\\Train_Face"
 
     resized_width = 64
     resized_height = 64
 
     size_of_batch = 128
-    epoch = 10000
+    epoch = 100000
     Z_dim = 100
 
     images_list = reader.read_data_list(train_dir_path, image_ext=".png")
@@ -56,28 +57,29 @@ def main():
     # normalize -1 to 1
     image = tf.scalar_mul(2.0 / 255.0, image) - tf.constant(1.0)
 
-    x = tf.train.shuffle_batch(tensors=[image], batch_size=size_of_batch, num_threads=4, capacity=50000, min_after_dequeue=10000)
+    x = tf.train.shuffle_batch(tensors=[image], batch_size=size_of_batch, num_threads=4, capacity=5000, min_after_dequeue=128)
 
     Z = tf.placeholder(tf.float32, shape=[None, Z_dim])
 
-    g_image = net.generator(Z)
+    g_image = net.generator(Z, is_train=True, is_reuse=False)
 
-    d_real, d_logit_real = net.discriminator(x)
-    d_fake, d_logit_fake = net.discriminator(g_image)
+    d_fake, d_logit_fake = net.discriminator(g_image, is_train=True, is_reuse=False)
+    d_real, d_logit_real = net.discriminator(x, is_train=True, is_reuse=True)
 
-    d_loss = -tf.reduce_mean(tf.log(d_real) + tf.log(1. - d_fake))
-    g_loss = -tf.reduce_mean(tf.log(d_fake))
-	
-	# d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(d_logit_real, tf.ones_like(d_logit_real)))
-	# d_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(d_logit_fake, tf.zeros_like(d_logit_fake)))
-	# d_loss = d_loss_real + d_loss_fake
-	# g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(d_logit_fake, tf.ones_like(d_logit_fake)))
 
-    # d_optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0000001).minimize(d_loss)
-    # g_optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0000001).minimize(g_loss)
+    # d_loss = -tf.reduce_mean(tf.log(d_real) + tf.log(1. - d_fake))
+    # g_loss = -tf.reduce_mean(tf.log(d_fake))
 
-    d_optimizer = tf.train.AdamOptimizer().minimize(d_loss)
-    g_optimizer = tf.train.AdamOptimizer().minimize(g_loss)
+    D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logit_real, labels=tf.ones_like(d_logit_real)))
+    D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logit_fake, labels=tf.zeros_like(d_logit_fake)))
+    d_loss = D_loss_real + D_loss_fake
+    g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logit_fake, labels=tf.ones_like(d_logit_fake)))
+
+    # d_optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(d_loss)
+    # g_optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(g_loss)
+
+    d_optimizer = tf.train.AdamOptimizer(learning_rate=0.0002).minimize(d_loss)
+    g_optimizer = tf.train.AdamOptimizer(learning_rate=0.0002).minimize(g_loss)
 
     init_op = tf.group(tf.global_variables_initializer(),
                        tf.local_variables_initializer())
@@ -88,22 +90,28 @@ def main():
         sess.run(init_op)
 
         coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(coord=coord)
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
         for iter_epoch in range(epoch):
-            if iter_epoch % 1000 == 0:
+            if iter_epoch % 100 == 0:
                 samples = sess.run(g_image, feed_dict={Z: sample_Z(16, Z_dim)})
 
                 samples += 1.0
                 samples = samples * (255.0 / 2.0)
+
+                # samples = samples * 255.0
 
                 fig = plot(samples)
                 plt.savefig('out/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
                 i += 1
                 plt.close(fig)
 
+
             _, batch_d_loss = sess.run([d_optimizer, d_loss], feed_dict={Z: sample_Z(size_of_batch, Z_dim)})
+            # _, batch_d_loss = sess.run([d_optimizer, d_loss], feed_dict={Z: sample_Z(size_of_batch, Z_dim)})
             _, batch_g_loss = sess.run([g_optimizer, g_loss], feed_dict={Z: sample_Z(size_of_batch, Z_dim)})
+            _, batch_g_loss = sess.run([g_optimizer, g_loss], feed_dict={Z: sample_Z(size_of_batch, Z_dim)})
+
 
 
 
